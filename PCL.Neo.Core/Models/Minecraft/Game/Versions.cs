5f0570dcf9ff0935c5f7b28f9f1c30f9e00c053a
WhiteCAT <64885812+whitecat346@users.noreply.github.com>
2025-06-29 14:59:01 +0800

    Revert "feature(game service): support vanilla and fabric launch"
    This reverts commit 23da5b6c611f8a2ba22ece9063b9ffcd87a48627.

--- PCL.Neo.Core/Models/Minecraft/Game/Versions.cs
+++ PCL.Neo.Core/Models/Minecraft/Game/Versions.cs
@@ -110,7 +110,8 @@ namespace PCL.Neo.Core.Models.Minecraft.Game
         /// </summary>
         public static async Task<VersionManifes?> GetVersionByIdAsync(string minecraftDirectory, string versionId)
         {
-            var versionJsonPath = Path.Combine(minecraftDirectory, $"{versionId}.json");
+            var versionDir = Path.Combine(minecraftDirectory, "versions", versionId);
+            var versionJsonPath = Path.Combine(versionDir, $"{versionId}.json");
 
             if (File.Exists(versionJsonPath))
             {
