de0419a9f0248df939b6c7d442a98934ec2e2a1e
WhiteCAT <64885812+whitecat346@users.noreply.github.com>
2025-07-16 00:33:42 +0800

    feature(http client pool): add auto dispose

--- PCL.Neo.Core/Utils/Net/HttpClientPool.cs
+++ PCL.Neo.Core/Utils/Net/HttpClientPool.cs
@@ -1,3 +1,5 @@
+using System.Collections.Concurrent;
+
 // ReSharper disable InconsistentNaming
 
 namespace PCL.Neo.Core.Utils.Net;
@@ -7,49 +9,125 @@ namespace PCL.Neo.Core.Utils.Net;
 /// </summary>
 internal static class HttpClientPool
 {
-    private static readonly Dictionary<string, HttpClient> _clients = new();
-    private static readonly Lock _lock = new();
+    private static readonly ConcurrentDictionary<string, HttpClientInfo> _clients = new();
+    private static readonly Timer _cleanupTimer;
+    private static readonly object _cleanupLock = new();
 
-    public static void AddHttpClient(string name, HttpClient client)
+    static HttpClientPool()
     {
-        lock (_lock)
+        // 每60秒检查一次过期的HttpClient
+        _cleanupTimer = new Timer(CleanupExpiredClients, null, TimeSpan.FromSeconds(60), TimeSpan.FromSeconds(60));
+    }
+
+    private class HttpClientInfo(HttpClient client, DateTime expirationTime)
+    {
+        public HttpClient Client { get; } = client;
+        public DateTime ExpirationTime { get; } = expirationTime;
+    }
+
+    /// <summary>
+    /// 添加HTTP客户端到池中，可以指定自动销毁时间
+    /// </summary>
+    /// <param name="name">客户端名称</param>
+    /// <param name="client">HTTP客户端实例</param>
+    /// <param name="lifetime">客户端生命周期，默认为1小时</param>
+    public static void AddHttpClient(string name, HttpClient client, TimeSpan? lifetime = null)
+    {
+        ArgumentNullException.ThrowIfNull(name);
+        ArgumentNullException.ThrowIfNull(client);
+
+        var expirationTime = DateTime.UtcNow.Add(lifetime ?? TimeSpan.FromSeconds(60));
+        var clientInfo = new HttpClientInfo(client, expirationTime);
+
+        if (!_clients.TryAdd(name, clientInfo))
         {
-            _clients.Add(name, client);
+            // 如果已存在同名客户端，先释放旧的再添加新的
+            if (_clients.TryRemove(name, out var oldClientInfo))
+            {
+                oldClientInfo.Client.Dispose();
+            }
+
+            _clients.TryAdd(name, clientInfo);
         }
     }
 
     /// <summary>
-    /// 获取指定名称的HTTP客户端实例，如果不存在则创建一个新的实例
+    /// 获取指定名称的HTTP客户端实例，如果不存在或已过期则创建一个新的实例
     /// </summary>
-    /// <param name="name"></param>
-    /// <returns></returns>
-    public static HttpClient GetClient(string name)
+    /// <param name="name">客户端名称</param>
+    /// <param name="lifetime">新建客户端的生命周期，默认为1小时</param>
+    /// <returns>HTTP客户端实例</returns>
+    public static HttpClient GetClient(string name, TimeSpan? lifetime = null)
     {
-        lock (_lock)
+        ArgumentNullException.ThrowIfNull(name);
+
+        if (_clients.TryGetValue(name, out var clientInfo))
         {
-            if (!_clients.TryGetValue(name, out var client))
+            if (DateTime.UtcNow < clientInfo.ExpirationTime)
             {
-                client = new HttpClient();
-                _clients[name] = client;
+                return clientInfo.Client;
             }
 
-            return client;
+            // 如果客户端已过期，移除并释放它
+            if (_clients.TryRemove(name, out var expiredClientInfo))
+            {
+                expiredClientInfo.Client.Dispose();
+            }
         }
+
+        // 创建新的客户端
+        var client = new HttpClient();
+        AddHttpClient(name, client, lifetime);
+        return client;
     }
 
     /// <summary>
     /// 释放指定名称的HTTP客户端实例
     /// </summary>
-    /// <param name="name"></param>
-    public static void ReleaseClient(string name)
+    /// <param name="name">客户端名称</param>
+    /// <returns>是否成功释放</returns>
+    public static bool ReleaseClient(string name)
     {
-        lock (_lock)
+        ArgumentNullException.ThrowIfNull(name);
+
+        if (_clients.TryRemove(name, out var clientInfo))
         {
-            if (_clients.ContainsKey(name))
+            clientInfo.Client.Dispose();
+            return true;
+        }
+
+        return false;
+    }
+
+    /// <summary>
+    /// 清理所有过期的HTTP客户端
+    /// </summary>
+    private static void CleanupExpiredClients(object? state)
+    {
+        // 使用锁防止多个清理操作同时进行
+        if (!Monitor.TryEnter(_cleanupLock))
+        {
+            return;
+        }
+
+        try
+        {
+            var now = DateTime.UtcNow;
+            var expiredClients = _clients.Where(kvp => now >= kvp.Value.ExpirationTime)
+                .Select(kvp => kvp.Key)
+                .ToList();
+
+            foreach (var name in expiredClients)
             {
-                _clients[name].Dispose();
-                _clients.Remove(name);
+                if (_clients.TryRemove(name, out var clientInfo))
+                {
+                    clientInfo.Client.Dispose();
+                }
             }
+        }
+        finally
+        {
+            Monitor.Exit(_cleanupLock);
         }
     }
 }
